0598aba9420a390354ee940d2fbf210e9d3e2dd3
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-12 17:52:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,21 @@
 def addition(num1, num2)
   add = num1 + num2
-  puts add
   return add
 end
 
 def subtraction(num1, num2)
-
+  sub = num1 * num2
+  return sub
 end
 
 def division(num1, num2)
-
+  div = num1 / num2
+  return div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  return mult
 end
 
 def modulo(num1, num2)
